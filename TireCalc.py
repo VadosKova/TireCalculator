e8c355b14dd30495b226475ad1858e2fbfce2400
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-26 10:24:09 +0200

    Created function for diameter

--- TireCalc.py
+++ TireCalc.py
@@ -7,3 +7,7 @@ class TireCalc:
 
     def calculate_sidewall_height(self):
         return self.tire.width * (self.tire.tire_profile / 100)
+
+    def calculate_diameter(self):
+        sidewall_height = self.calculate_sidewall_height()
+        return (sidewall_height * 2) + (self.tire.diameter * 25.4)
