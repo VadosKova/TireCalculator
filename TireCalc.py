606c91a8262dd87b67eca2aeecf7637811b9550e
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-26 10:18:25 +0200

    Created function for sidewall height

--- TireCalc.py
+++ TireCalc.py
@@ -4,3 +4,6 @@ from Tire import Tire
 class TireCalc:
     def __init__(self, tire):
         self.tire = tire
+
+    def calculate_sidewall_height(self):
+        return self.tire.width * (self.tire.tire_profile / 100)
