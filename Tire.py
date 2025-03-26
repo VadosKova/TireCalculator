3542ae9b5dfefbeaa8ffb6d467a8cdb76cd95a27
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-26 10:12:09 +0200

    Created function for getting calculation result

--- Tire.py
+++ Tire.py
@@ -3,3 +3,16 @@ class Tire:
         self.width = width
         self.tire_profile = tire_profile
         self.diameter = diameter
+
+    def calc_result(self, tire_calc):
+        sidewall_height = tire_calc.calculate_sidewall_height()
+        diameter = tire_calc.calculate_diameter()
+        circumference = tire_calc.calculate_circumference()
+        revolutions = tire_calc.calculate_revolutions_per_km()
+
+        return (f"Шина {self.width}/{self.tire_profile} R{self.diameter}:\n"
+                f"  Высота боковины: {sidewall_height:.2f} мм\n"
+                f"  Диаметр обода: {self.diameter * 25.4:.2f} мм\n"
+                f"  Общий диаметр: {diameter:.2f} мм\n"
+                f"  Длина окружности: {circumference:.2f} мм\n"
+                f"  Число оборотов на 1 км: {revolutions:.2f} оборотов")
