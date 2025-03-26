610d0c529ae6281091e6d6a435ef42b533ead97c
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-26 11:19:32 +0200

    Tweaked the field

--- AlgorithmTest.py
+++ AlgorithmTest.py
@@ -4,10 +4,10 @@ from TireCalc import TireCalc
 print("---Введите параметры шины---")
 
 width = int(input("Ширина шины (мм): "))
-aspect_ratio = int(input("Высота боковины (%): "))
+tire_profile = int(input("Профиль шины (%): "))
 diameter = int(input("Диаметр обода (дюймы): "))
 
-tire = Tire(width, aspect_ratio, diameter)
+tire = Tire(width, tire_profile, diameter)
 tire_calc = TireCalc(tire)
 
 res = tire.calc_result(tire_calc)
